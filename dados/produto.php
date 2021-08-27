2b6f9bbb5f542918d0505817a1162eb304f0fae0
VitorCBazzani <89615715+VitorCBazzani@users.noreply.github.com>
2021-08-27 20:19:38 -0300

    Add files via upload

---
+++ dados/produto.php
@@ -0,0 +1,23 @@
+<?php
+
+$produtos[] = array(
+	'codigo'=> 'produto 1',
+	'nome'  => 'cookie caseiro',
+	'descricao' => 'Delicioso cookie preparado pela nossa melhor confeiteira',
+	'preco' => 11.99,
+	'imagem' => 'cookie1.png',
+);
+$produtos[] = array(
+	'codigo'=> 'produto 2',
+	'nome'  => 'cookie gotas de chocolate',
+	'descricao' => 'Delicioso cookie preparado pela nossa melhor confeiteira',
+	'preco' => 15.99,
+	'imagem' => 'cookie1.png',
+);
+$produtos[] = array(
+	'codigo'=> 'coookie Red Vavelt',
+	'nome'  => 'cookie caseiro',
+	'descricao' => 'Delicioso cookie preparado pela nossa melhor confeiteira',
+	'preco' => 13.99,
+	'imagem' => 'cookie1.png',
+);
